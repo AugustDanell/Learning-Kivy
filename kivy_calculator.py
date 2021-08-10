0e63b83012f3934b7b528fbd8a50675d0efda46a
August Danell Håkansson <augustdanellhakansson96@gmail.com>
2021-08-10 16:48:26 +0200

    Calculator functionality and error handling.

--- kivy_calculator.py
+++ kivy_calculator.py
@@ -1,5 +1,6 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.uix.button import Button
@@ -42,10 +43,32 @@ class text_game(App):
     def get_table_size(self, current_state):
         return len(self.get_transistion_table(current_state))
 
+    def calculate(self):
+        try:
+            a = int(self.variable_one.text)
+            b = int(self.variable_two.text)
+            operations = {
+                1: a + b,
+                2: a - b,
+                3: a * b,
+                4: a / b,
+            }
+
+            self.scenario_text.text = "Answer is: " + str(operations[self.state])
+        except ZeroDivisionError:
+            self.scenario_text.text = "Please make no divisions with zero! Try again!"
+        except ValueError:
+            if(self.variable_two.text == ""):
+                self.scenario_text.text = "Please let b be a real value!"
+            elif(self.variable_one.text == ""):
+                self.scenario_text.text = "Set a value for numerator a!"
+            else:
+                self.scenario_text.text = "Please only use numeric values"
 
 
     def build(self):
         self.window = GridLayout()
+        self.button_box = BoxLayout()
         self.window.cols = 1
         self.window.size_hint = (0.6, 0.7)
         self.window.pos_hint = {"center_x": 0.5, "center_y": 0.5}
@@ -95,6 +118,31 @@ class text_game(App):
         )
         self.window.add_widget(self.d)
         self.d.bind(on_press=lambda x: self.update_state("d"))
+
+        self.variable_one = TextInput(
+                    multiline= False,
+                    padding_y= (12,20),
+                    size_hint= (1, 0.5)
+                    )
+
+        self.variable_two = TextInput(
+                    multiline= False,
+                    padding_y= (12,20),
+                    size_hint= (1, 0.5)
+                    )
+
+        self.window.add_widget(self.variable_one)
+        self.window.add_widget(self.variable_two)
+
+        self.calculate_button = Button(
+            text="Calculate",
+            size_hint=(1, 0.5),
+            bold=True,
+            background_color='#00FFAF',
+        )
+
+        self.calculate_button.bind(on_press=lambda x: self.calculate())
+        self.window.add_widget(self.calculate_button)
 
         return self.window
 
