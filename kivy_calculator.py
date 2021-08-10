20d3adbee529a6d5b8b376e6b8d269beaf8b542c
August Danell Håkansson <augustdanellhakansson96@gmail.com>
2021-08-10 15:30:16 +0200

    Adding initial files for a simple calculator.

---
+++ kivy_calculator.py
@@ -0,0 +1,103 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+
+class text_game(App):
+    def text_bank(self):
+        label = {
+            1: "Operation is set to addition!",
+            2: "Operation is set to subtraction!",
+            3: "Operation is set to multiplication!",
+            4: "Operation is set to division!"
+        }
+
+        return label[self.state]
+
+    def update(self):
+        self.scenario_text.text = self.text_bank()
+
+    ''' get_transition_table
+        A general function that returns the transitions for a state s in the form of a list.     
+    '''
+
+    def update_state(self,  button_id):
+        index =  ord(button_id) - 97
+        game_states = {
+                1 : [1,2,3,4],
+                2 : [1,2,3,4],
+                3 : [1,2,3,4],
+                4 : [1,2,3,4],
+        }
+        self.state = game_states[self.state][index]
+        self.update()
+
+    ''' get_table_size
+        Given that we are in a state s we need to know the size of that state table that is how many transitions there
+        are, we need this to be able to know how many buttons to draw for a given state.
+    '''
+
+    def get_table_size(self, current_state):
+        return len(self.get_transistion_table(current_state))
+
+
+
+    def build(self):
+        self.window = GridLayout()
+        self.window.cols = 1
+        self.window.size_hint = (0.6, 0.7)
+        self.window.pos_hint = {"center_x": 0.5, "center_y": 0.5}
+        self.state = 1
+
+        self.scenario_text = Label(
+                            text= self.text_bank(),
+                            font_size= 18,
+                            color= '#CFFFFF'
+                            )
+
+        self.window.add_widget(self.scenario_text)
+
+        # Adding buttons:
+        self.a = Button(
+            text="Set Addition",
+            size_hint=(1, 0.5),
+            bold=True,
+            background_color='#00FFAF',
+        )
+        self.window.add_widget(self.a)
+        self.a.bind(on_press=lambda x: self.update_state("a"))
+
+        self.b = Button(
+            text="Set Subtraction",
+            size_hint=(1, 0.5),
+            bold=True,
+            background_color='#00FFAF',
+        )
+        self.window.add_widget(self.b)
+        self.b.bind(on_press=lambda x: self.update_state("b"))
+
+        self.c = Button(
+            text="Set Multiplication",
+            size_hint=(1, 0.5),
+            bold=True,
+            background_color='#00FFAF',
+        )
+        self.window.add_widget(self.c)
+        self.c.bind(on_press=lambda x: self.update_state("c"))
+
+        self.d = Button(
+            text="Set Division",
+            size_hint=(1, 0.5),
+            bold=True,
+            background_color='#00FFAF',
+        )
+        self.window.add_widget(self.d)
+        self.d.bind(on_press=lambda x: self.update_state("d"))
+
+        return self.window
+
+
+if __name__ == "__main__":
+    text_game().run()
