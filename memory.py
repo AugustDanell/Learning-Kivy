de42da9ddc1afe055d8266c4aa611fdff237770b
August Danell Håkansson <augustdanellhakansson96@gmail.com>
2021-08-12 20:40:06 +0200

    Setting up game logics, cards and shuffling them.

--- memory.py
+++ memory.py
@@ -1,13 +1,20 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+
 class card:
     def __init__(self, type):
         self.type = type
 
         switch = {
-            "duck" : "memory_assets/duck.png",
-            "dog"  : "memory_assets/dog.png",
-            "cat"  : "memory_assets/cat.png",
-            "horse": "memory_assets/horse.png",
-            "elephant": "memory_assets/elephant.png",
+            1 : "memory_assets/duck.png",
+            2 : "memory_assets/dog.png",
+            3 : "memory_assets/cat.png",
+            4 : "memory_assets/horse.png",
+            5 : "memory_assets/elephant.png",
         }
         try:
             self.link = switch[type]
@@ -20,6 +27,21 @@ class card:
             return True
         return False
 
+class Memory(App):
+    def fisher_yates(self, list_to_shuffle):
+        pass
+    
+    def build(self):
+        amount_of_cards = 5
+        card_list = []
+        
+        for i in range(amount_of_cards):
+            card_list.append(card(i+1))
+            card_list.append(card(i+1))
+        
+        self.fisher_yates(card_list)
+            
+
+
 if __name__ == "__main__":
-    amount_of_cards = 5
-    #text_game().run()
+    Memory().run()
