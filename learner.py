e6dff293343272b31f32beeddc32e4dbfbb32943
August Danell Håkansson <augustdanellhakansson96@gmail.com>
2021-08-14 20:58:51 +0200

    A working (but ugly) main menu, now bind the buttons.

--- learner.py
+++ learner.py
@@ -16,20 +16,37 @@ class Learner(App):
     def quit(self):
         App.get_running_app().stop()
 
+    def load_cards(self):
+        pass
+
+    def set_cards(self):
+        pass
+
     def main_menu(self):
         initial_welcome = Label(
             text = "Hello and welcome to the learner app",
         )
 
-
-        quit_button = Button(
-            text = "quit",
+        self.start = Button(
+            text = "Set Flashcards",
         )
 
-        quit_button.bind(on_press=lambda x: self.quit())
+        self.load = Button(
+            text = "Load Flashcards (not in use yet)",
+        )
+
+        self.quit_button = Button(
+            text = "Quit",
+        )
+
+        self.load.bind(on_press=lambda x: self.load_cards())
+        self.quit_button.bind(on_press=lambda x: self.quit())
+        self.start.bind(on_press=lambda x: self.set_cards())
 
         self.window.add_widget(initial_welcome)
-        self.window.add_widget(quit_button)
+        self.window.add_widget(self.start)
+        self.window.add_widget(self.load)
+        self.window.add_widget(self.quit_button)
 
     def build(self):
         # State
