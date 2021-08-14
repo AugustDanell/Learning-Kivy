4731e84fa506c2e18cbacb72d72f08d96905a854
August Danell Håkansson <augustdanellhakansson96@gmail.com>
2021-08-14 22:21:42 +0200

    Adding more views.

--- learner.py
+++ learner.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.image import Image
+from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 import random
 import time
@@ -19,8 +20,57 @@ class Learner(App):
     def load_cards(self):
         pass
 
-    def set_cards(self):
+    def play_game(self):
         pass
+    
+    def create_flashcard(self):
+        if(self.flashcards_left == 0):
+            self.play_game()        
+        else:
+            pass
+        
+    def extract_from_text_field(self):
+        try:
+            self.flashcards = int(self.text_form.text)
+            self.window.clear_widgets()
+            if(self.flashcards <= 0):
+                raise Exception
+            else:
+                self.flashcard_list = []
+                self.flashcards_left = self.flashcards
+                self.create_flashcard()
+        except:
+            self.flashcards = 0
+            self.window.clear_widgets()
+            self.label.text = "Please, enter in a valid integer greater than zero, how many flashcards do you want?"
+            self.handle_integer_input()
+
+    def handle_integer_input(self):
+        self.window.add_widget(self.label)
+
+        self.text_form = TextInput(
+            multiline=False,
+        )
+
+        self.window.add_widget(self.text_form)
+
+        self.proceed = Button(
+            text="Proceed",
+        )
+
+        self.window.add_widget(self.proceed)
+        self.flashcards = 0
+        self.proceed.bind(on_press= lambda x: self.extract_from_text_field())
+
+    def set_cards(self):
+        self.window.clear_widgets() # Clears widgets from the previous view.
+        self.counter = 0            # Initiate a counter that alternates back and forth so we can write on the back of the card.
+
+        self.label = Label(
+            text = "How many flashcards do you want to make (enter in a number and the proceed button)?",
+        )
+
+        self.handle_integer_input()
 
     def main_menu(self):
         initial_welcome = Label(
