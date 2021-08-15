16bbd139e90a86e533a16a9e37d412b8a99326ee
August Danell Håkansson <augustdanellhakansson96@gmail.com>
2021-08-15 15:36:35 +0200

    Adding a little bit more work.

--- real_calculator.py
+++ real_calculator.py
@@ -13,67 +13,79 @@ class card:
 
 
 class base(App):
+    def bind_numbers_with(self, number):
+        pass
+
     def fill_buttons(self):
         self.one = Button(
-            
+            text = "1",
         )
+        self.one.bind(on_press= lambda x : self.bind_numbers_with(1))
 
         self.two = Button(
-
+            text="2",
         )
+        self.two.bind(on_press= lambda x : self.bind_numbers_with(2))
 
         self.three = Button(
-
+            text="3",
         )
+        self.three.bind(on_press= lambda x : self.bind_numbers_with(3))
 
         self.four = Button(
-
+            text="4",
         )
+        self.four.bind(on_press= lambda x : self.bind_numbers_with(4))
 
         self.five = Button(
-
+            text="5",
         )
+        self.five.bind(on_press= lambda x : self.bind_numbers_with(5))
 
         self.six = Button(
-
+            text="6",
         )
+        self.six.bind(on_press= lambda x : self.bind_numbers_with(6))
 
         self.seven = Button(
-
+            text="7",
         )
+        self.seven.bind(on_press= lambda x : self.bind_numbers_with(7))
 
         self.eight = Button(
-
+            text="8",
         )
+        self.eight.bind(on_press= lambda x : self.bind_numbers_with(8))
 
         self.nine = Button(
-
+            text="9",
         )
+        self.nine.bind(on_press= lambda x : self.bind_numbers_with(9))
 
         self.div = Button(
-
+            text="/",
         )
 
         self.mult = Button(
-
+            text="*",
         )
 
         self.sub = Button(
-
+            text="-",
         )
 
         self.add = Button(
-
+            text="+",
         )
 
         self.zero = Button(
-
+            text="0",
         )
 
         self.equal = Button(
-
+            text="=",
         )
-        
+
     def build(self):
 
         # Fixing a grid:
@@ -81,11 +93,11 @@ class base(App):
         self.window.cols = 1
         self.button_window = GridLayout()
         self.button_window.cols = 4
-        
+
         self.answer = Label(
             text = 0,
         )
-        
+
         return self.window
 
 
